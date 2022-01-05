26394286b6e068ff33398c5070c812d5bd5a7cf0
cdufour <opusidea@gmail.com>
2022-01-05 14:37:14 +0100

    script4

--- scripts/script4.js
+++ scripts/script4.js
@@ -10,6 +10,20 @@ const h2 = $("#h2");
 students.forEach((s) => {
   
   // créé un paragraphe
-  let p = $("<p>");
+  let p = $("<p>" + s.name + "</p>");
+
+  // si étudiant a note inférieure à 10, changement css
+  if (s.grade1 < 10) {
+    p.text(s.name + " (mauvais élève)");
+    p.css("color", "red");
+  }
+
+  // écoute l'événement clicj
+  p.click(() => {
+    h2.text(s.name + " a obtenu la note de: " + s.grade1);
+  })
+
+  // insère la pagraphe dans body
+  $("body").append(p);
 
 })
